be5cd8a9f2f00eff90975fbdae8fb8a206b4dde7
Gabriel Nelson <nel13776@morris.umn.edu>
2026-01-15 13:10:07 -0600

    Add greetings for Gabe and Elijah
    Added a new 'gabeSaysHello()' and 'elijahSaysHello()' methods, and added calls to those methods in the correct location in 'generatedOutput()'.

--- src/main/java/hellos/Hellos.java
+++ src/main/java/hellos/Hellos.java
@@ -9,18 +9,27 @@ public class Hellos {
   public String generateOutput() {
     StringBuilder builder = new StringBuilder();
 
+    builder.append(elijahSaysHello());
+    builder.append(gabeSaysHello());
     builder.append(kkSaysHello());
     builder.append(nicSaysHello());
 
     return builder.toString();
   }
 
-  private String nicSaysHello() {
-    return "Nic says 'Howdy!'\n";
+  private String elijahSaysHello() {
+    return "Elijah says 'Hello People!'\n";
+  }
+
+  private String gabeSaysHello() {
+    return "Gabe says 'Hi Classmates!'\n";
   }
 
   private String kkSaysHello() {
     return "KK says 'Hello!'\n";
   }
 
+  private String nicSaysHello() {
+    return "Nic says 'Howdy!'\n";
+  }
 }
